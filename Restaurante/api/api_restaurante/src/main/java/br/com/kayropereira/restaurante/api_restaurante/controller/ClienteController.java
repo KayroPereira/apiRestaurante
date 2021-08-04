6e7f05fc65302180f1010f64208906964a80f2ea
Kayro Pereira <kayrofellyxbr@gmail.com>
2021-08-03 22:03:07 -0300

    Create client finished

---
+++ Restaurante/api/api_restaurante/src/main/java/br/com/kayropereira/restaurante/api_restaurante/controller/ClienteController.java
@@ -0,0 +1,26 @@
+package br.com.kayropereira.restaurante.api_restaurante.controller;
+
+import br.com.kayropereira.restaurante.api_restaurante.dto.request.ClienteDTO;
+import br.com.kayropereira.restaurante.api_restaurante.dto.response.MessageResponseDTO;
+import br.com.kayropereira.restaurante.api_restaurante.service.ClienteService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/cliente")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class ClienteController {
+
+    private ClienteService clienteService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createCliente(@RequestBody @Valid ClienteDTO clienteDTO){
+        System.out.println(clienteDTO);
+        return clienteService.createCliente(clienteDTO);
+    }
+}
--- Restaurante/api/api_restaurante/src/main/java/br/com/kayropereira/restaurante/api_restaurante/entity/Telefone.java
+++ Restaurante/api/api_restaurante/src/main/java/br/com/kayropereira/restaurante/api_restaurante/entity/Telefone.java
@@ -1,16 +1,11 @@
 package br.com.kayropereira.restaurante.api_restaurante.entity;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Data
@@ -18,31 +13,20 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "telefone")
-//@JsonIdentityInfo(
-//        generator = ObjectIdGenerators.PropertyGenerator.class,
-//        property = "id")
 public class Telefone {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tf_id")
     private Integer id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "tf_numero")
     private String numero;
 
-    @Column(name = "fk_tipo_telefone_id", nullable = false)
+    @Column(name = "tp_tf_id", nullable = false)
     private Long idTipoTelefone;
 
-//    @ManyToMany(targetEntity = ClienteAvulso.class, cascade = CascadeType.ALL)
-////    @ManyToMany(targetEntity = ClienteAvulso.class, mappedBy = "telefones", cascade = CascadeType.ALL)
-////    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST})
-////    @OneToMany(fetch = FetchType.LAZY)
-//    @JoinTable(name = "cliente_avulso_telefone",
-//    joinColumns = @JoinColumn(name = "fk_telefone_id"),
-//    inverseJoinColumns = @JoinColumn(name = "fk_cliente_avulso_id"))
-//    private List<ClienteAvulso> clientesAvulsos = new ArrayList<>();
-
-    @ManyToMany(mappedBy = "telefones", fetch = FetchType.LAZY)
-    @JsonIgnoreProperties("telefones")
-    private List<ClienteAvulso> clientesAvulsos;
+//    @ManyToMany(mappedBy = "telefones", fetch = FetchType.LAZY)
+//    @JsonIgnoreProperties("telefones")
+//    private List<ClienteAvulso> clientesAvulsos;
 }
