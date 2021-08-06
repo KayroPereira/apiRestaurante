5ad542bc38c42b7e6639e920c6a4ee6f1d547966
Kayro Pereira <kayrofellyxbr@gmail.com>
2021-08-06 20:48:10 -0300

    Atualizado modelo DB

---
+++ Restaurante/api/api_restaurante/src/main/java/br/com/kayropereira/restaurante/api_restaurante/entity/GrupoBairro.java
@@ -0,0 +1,10 @@
+package br.com.kayropereira.restaurante.api_restaurante.entity;
+
+public class GrupoBairro {
+
+    private Long id;
+
+    private String descricao;
+
+    private Float fatorCobranca;
+}
